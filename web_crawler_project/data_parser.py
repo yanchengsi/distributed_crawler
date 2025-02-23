7d4a60f6413ada7bcf4217e7dd418f4eadd23ea9
yanchengsi <ab7766554433@qq.com>
2025-02-23 16:44:56 +0800

    Delete web_crawler_project directory
    删除不必要文件夹

--- web_crawler_project/data_parser.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-from data_storage import data_storage
-
-def data_parser(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    # 这里可以根据具体需求提取信息
-    texts = soup.get_text()
-    data_storage(texts)
